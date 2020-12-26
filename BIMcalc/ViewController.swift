50ea6934e9d6ba5002326d87bd96d57de42e9130
kaito <kaitooshiro1@icloud.com>
2020-12-26 13:44:23 +0900

    Initial Commit

---
+++ BIMcalc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BIMcalc
+//
+//  Created by 大城海斗 on 2020/12/26.
+//  Copyright © 2020 大城海斗. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
