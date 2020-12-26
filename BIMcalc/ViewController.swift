a01ca776accdec9077e15e9abbb4768db1604113
kaito <kaitooshiro1@icloud.com>
2020-12-26 21:36:42 +0900

    readme追加

--- BIMcalc/ViewController.swift
+++ BIMcalc/ViewController.swift
@@ -10,11 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var height: UITextField!
+    
+    @IBOutlet weak var weight: UITextField!
+    
+    @IBOutlet weak var bmiText: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.height.keyboardType = UIKeyboardType.numberPad
+        self.weight.keyboardType = UIKeyboardType.numberPad
     }
+    @IBAction func resetButton(_ sender: Any) {
+        height.text = ""
+        weight.text = ""
+    }
+    
 
-
+    @IBAction func calcButton(_ sender: Any) {
+        let dheight = Int(height.text!)
+        let dweight = Int(weight.text!)
+        let dheight2 = dheight! * dheight!
+        let bmiInt = Int(dweight! * 10000 / dheight2)
+        let bmi = String(bmiInt)
+    
+        bmiText.text = ("あなたのBMIは" + bmi + "です")
+    }
+    
 }
 
